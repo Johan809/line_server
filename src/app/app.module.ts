ca0a58ccb1f3cf66c9798cf9c234664c33f38fbc
k0o <sb.knot@gmail.com>
2021-06-23 20:52:36 -0400

    env configs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { UserAdressModule } from './user-adress/user-adress.module';
+import { AppController } from 'app/app.controller';
+import { AppService } from 'app/app.service';
+import { UserModule } from 'app/user/user.module';
+import { UserAdressModule } from 'app/user-adress/user-adress.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule, UserAdressModule],
+  imports: [
+    UserModule,
+    UserAdressModule,
+    GraphQLModule.forRoot({
+      playground: process.env.NODE_ENV === 'development',
+      autoSchemaFile: join(__dirname, 'src/schema.gql'),
+      sortSchema: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	const port = process.env.PORT || 3000;
+	await app.listen(port);
+	console.log(`Running on ::${port}`);
 }
+
 bootstrap();
